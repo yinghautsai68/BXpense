ebae2b68d952bb5b0bae6b7bc6ebfecf55d0a054
yinghau68 <yinghautsai68cycu@gmail.com>
2026-04-13 15:44:42 +0800

    feat: update DetailLayout - change back button to x - change Title to Subtitle - add navigation function to back button

--- frontend/src/layout/DetailLayout.tsx
+++ frontend/src/layout/DetailLayout.tsx
@@ -1,16 +1,18 @@
 import React from 'react'
-import { Title } from '../components/Typography';
+import { SubTitle } from '../components/Typography';
+import { useNavigate } from 'react-router-dom';
 
 type DetailLayoutProps = {
     title: string,
     children: React.ReactNode
 };
 const DetailLayout = ({ title, children }: DetailLayoutProps) => {
+    const navigate = useNavigate();
     return (
         <div className="flex flex-col gap-4 w-full min-h-screen px-3 pt-3 pb-5 bg-gray-200">
-            <div className="flex flex-row justify-between items-center">
-                <p className="w-full">previous</p>
-                <Title className="w-full text-center">{title}</Title>
+            <div className="flex flex-row justify-between items-center px-3">
+                <p onClick={() => navigate(-1)} className="w-full font-bold">x</p>
+                <SubTitle className="w-full text-center">{title}</SubTitle>
                 <div className="w-full"></div>
             </div>
             {children}
