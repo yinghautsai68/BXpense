db2481044e6caa2f61adf1dbb91fd298707a9a43
yinghau68 <yinghautsai68cycu@gmail.com>
2026-04-15 00:05:11 +0800

    feat: add line chart analytics endpoint and update routes

--- backend/src/modules/analytics/analytics.controller.ts
+++ backend/src/modules/analytics/analytics.controller.ts
@@ -1,5 +1,73 @@
+import { record, success } from "zod";
 import { db } from "../../config/db";
 import type { Request, Response } from "express";
+
+export const getLineChartSummaryExpense = async (req: Request, res: Response) => {
+    try {
+        const { user_id } = (req as any).user;
+        /*
+        const [result]: any = await db.query(
+            `
+            SELECT
+                id,
+                user_id,
+                record_date,
+                type,
+                amount
+            FROM records 
+            WHERE user_id = ?
+            `,
+            [user_id]
+        )
+        if (result.length === 0) {
+            return res.status(404).json({ success: false, message: '沒有資料', data: [] });
+        }
+
+        const formatResult = result.map((data: any) => ({
+            ...data, record_date: data.record_date.toISOString().split("T")[0]
+        }));
+
+        const group: Record<string, any[]> = {};
+        for (let i = 0; i < formatResult.length; i++) {
+            if (!group[formatResult[i].record_date]) {
+                group[formatResult[i].record_date] = [];
+            }
+
+            group[formatResult[i].record_date]!.push({
+                id: formatResult[i].id,
+                user_id: formatResult[i].user_id,
+                type: formatResult[i].type,
+                amount: formatResult[i].amount
+            });
+        }
+
+        const chartData = Object.entries(group).map(([date, items]) => {
+            const total = items.reduce((sum, item) => sum + Number(item.amount), 0);
+
+            return {
+                date,
+                amuont: total
+            }
+        });
+        */
+        const [result]: any = await db.query(
+            `
+            SELECT 
+                DATE_FORMAT(record_date, '%Y-%m-%d') AS date,
+                SUM(amount) AS amount
+            FROM records
+            WHERE user_id = ?
+            GROUP BY date
+            ORDER BY date ASC
+            `,
+            [user_id]
+        );
+        res.status(200).json({ success: true, message: '資料', data: result })
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, message: `SERVER ERROR` });
+    }
+}
 
 export const getCategorySummary = async (req: Request, res: Response) => {
     try {
--- backend/src/modules/analytics/analytics.routes.ts
+++ backend/src/modules/analytics/analytics.routes.ts
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getCategorySummary } from './analytics.controller';
+import { getCategorySummary, getLineChartSummaryExpense } from './analytics.controller';
 import { protect } from '../../middleware/authenticate';
 
 const analyticsRouter = express.Router();
 
+analyticsRouter.get('/line', protect, getLineChartSummaryExpense);
 analyticsRouter.get('/', protect, getCategorySummary);
 
 export default analyticsRouter;
