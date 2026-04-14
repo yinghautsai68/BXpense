7fa7511052d6065b1a1f5b99cdcd9da4cf2bdb33
yinghau68 <yinghautsai68cycu@gmail.com>
2026-04-15 00:29:25 +0800

    feat: update ExpenseChart layout - update XAxis height

--- frontend/src/components/ExpenseChart.tsx
+++ frontend/src/components/ExpenseChart.tsx
@@ -10,7 +10,7 @@ const ExpenseChart = ({ data }: ExpenseChartProps) => {
         <ResponsiveContainer width="100%" height="100%">
             <LineChart data={data} >
                 <CartesianGrid strokeDasharray='10 10' />
-                <XAxis height={10} tick={{ fontSize: 10 }} dataKey="date" />
+                <XAxis height={35} tick={{ fontSize: 10 }} dataKey="date" />
                 <YAxis width={25} tick={{ fontSize: 10 }} />
                 <Line dataKey='amount' type='monotone' strokeWidth={3} stroke='orange' />
                 <Tooltip />
