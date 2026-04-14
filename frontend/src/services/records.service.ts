429548e11e39ba2059dadce2c6c12e85dc5771e8
yinghau68 <yinghautsai68cycu@gmail.com>
2026-04-14 19:09:50 +0800

    feat: add get top 10 expenses service

--- frontend/src/services/records.service.ts
+++ frontend/src/services/records.service.ts
@@ -146,3 +146,24 @@ export const deleteById = async (token: string, recordId: string) => {
         throw error;
     }
 }
+
+export const getTopExpenseRecords = async (token: string) => {
+    try {
+        const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/records/top-expenses`, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${token}`
+            },
+        });
+        const result = await response.json();
+        if (!result.success) {
+            throw new Error(result.message);
+        }
+
+        return result.data;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
+
