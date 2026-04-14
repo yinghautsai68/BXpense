ebd460770952d3c7e10d78c109d85b81b3cbdb3b
yinghau68 <yinghautsai68cycu@gmail.com>
2026-04-15 00:14:22 +0800

    feat: line chart and category summary data in Analytics - show line chart (date vs amount) - display category ranking and percentage

--- frontend/src/pages/Analysis.tsx
+++ frontend/src/pages/Analysis.tsx
@@ -3,10 +3,72 @@ import ExpenseCard from '../components/ExpenseCard'
 import { getTopExpenseRecords } from '../services/records.service';
 import { useAuth } from '../context/AuthContext';
 import type { RecordType } from '../types/records.type';
+import ExpenseChart from '../components/ExpenseChart';
+import ExpensePieChart from '../components/ExpensePieChart';
+import { getCategorySummary, getLine } from '../services/analytics.service';
+import Card from '../components/Card';
+import { categoryZhTW } from '../constants/categoryZhTW';
 
 const Analysis = () => {
     const { token } = useAuth();
     const [topExpenses, setTopExpenses] = useState<RecordType[]>([]);
+    const [line, setLine] = useState([]);
+    const [categorySummary, setCategorySummary] = useState([]);
+
+
+    useEffect(() => {
+        if (!token) {
+            return;
+        }
+        const fetchLine = async () => {
+            try {
+                const data = await getLine(token);
+                console.log(data);
+                setLine(data);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        fetchLine();
+    }, [token])
+
+
+    const totalAmount = categorySummary.reduce((sum, item) => sum + Number(item.total_amount), 0);
+    const getPercent = (amount: number) => {
+        return ((amount / totalAmount) * 100).toFixed(2);
+    }
+
+
+
+    const [pieData, setPieData] = useState([]);
+    useEffect(() => {
+
+        let pie = categorySummary.map((item, index) => (
+            {
+                name: item.name,
+                value: Number(item.total_amount)
+            }
+        ));
+        setPieData(pie);
+    }, [categorySummary]);
+
+    useEffect(() => {
+        if (!token) {
+            return;
+        }
+        const fetchCategorySummary = async () => {
+            try {
+                const data = await getCategorySummary(token);
+                console.log(data);
+                setCategorySummary(data);
+
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        fetchCategorySummary();
+    }, [token]);
+
     useEffect(() => {
         if (!token) {
             return;
@@ -22,6 +84,8 @@ const Analysis = () => {
         };
         fetchTopExpenseRecords();
     }, [token]);
+
+
     return (
         <>
             <div className='flex flex-row items-center'>
@@ -29,14 +93,45 @@ const Analysis = () => {
                 <button>支出</button>
             </div>
 
-            <div className='w-full h-[180px] bg-white'>
+            <Card className='w-full h-[280px] bg-white'>
+                <ExpenseChart data={line}></ExpenseChart>
+            </Card>
 
-            </div>
-
-            <div className='w-full h-[180px] bg-white'>
-
-            </div>
-
+            <Card>
+                <div className='w-full h-[180px] bg-white'>
+                    <ExpensePieChart data={pieData} />
+                </div>
+                <div className='flex flex-col  gap-5 w-full'>
+                    {
+                        categorySummary.map((category, index) => (
+                            <div key={index} className='flex flex-row  items-center gap-2 w-full '>
+                                <img src={category.image_url} alt='category_img' className='w-10 aspect-square p-2  rounded-full bg-gray-100' />
+                                <div className='flex flex-col w-full'>
+                                    <div className='flex flex-row'>
+                                        <div className='flex flex-col'>
+                                            <div className='flex-1 flex flex-row items-center gap-1 text-sm font-bold'>
+                                                <span>{index + 1}</span>
+                                                <span>{categoryZhTW[category.name]}</span>
+                                            </div>
+                                            <span className='text-xs'>{getPercent(category.total_amount)}</span>
+                                        </div>
+                                        <div className='flex-2 flex flex-col items-end text-xs'>
+                                            <span className='text-end font-bold'>-NT$ {category.total_amount}</span>
+                                            <span>{category.count}筆</span>
+                                        </div>
+                                    </div>
+                                    <div className='w-full h-2 rounded-xl bg-gray-200'>
+                                        <div style={{
+                                            width: `${getPercent(Number(category.total_amount))}%`
+                                        }} className={`h-2 rounded-xl bg-red-500`}>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        ))
+                    }
+                </div>
+            </Card>
             <div className='flex flex-col w-full p-4 bg-white rounded-xl '>
                 <div className='flex flex-row justify-between items-center'>
                     <span>單筆金額排行</span>
