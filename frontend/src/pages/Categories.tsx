7523dc08df914565178cfc3f7b355b0d34af2b5d
yinghau68 <yinghautsai68cycu@gmail.com>
2026-04-15 23:06:50 +0800

    feat: update Categories to fetch categories and update UI - add fetch existing categories feature - update UI to show icon

--- frontend/src/pages/Categories.tsx
+++ frontend/src/pages/Categories.tsx
@@ -1,9 +1,13 @@
-import { useState, type ChangeEvent } from 'react';
+import { useEffect, useState, type ChangeEvent } from 'react';
 import Card from '../components/Card'
 import Icon from '../components/Icon'
-import type { CreateCategoryType } from '../types/categories.type';
+import type { CategoryType, CreateCategoryType } from '../types/categories.type';
+import { useAuth } from '../context/AuthContext';
+import { getCategories } from '../services/categories.service';
 
 const Categories = () => {
+    const { token, user } = useAuth();
+
     const [categoryForm, setCategoryForm] = useState<CreateCategoryType>({
         name: '',
         image_url: ''
@@ -14,6 +18,28 @@ const Categories = () => {
         console.log(name, value)
         setCategoryForm((prev) => ({ ...prev, [name]: value }));
     }
+
+    const [categories, setCategories] = useState<CategoryType[]>([]);
+    useEffect(() => {
+        if (!token || !user) {
+            return;
+        }
+        const fetchCategories = async () => {
+            try {
+                const data = await getCategories(token, user.userId);
+                console.log(data);
+                setCategories(data);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        fetchCategories();
+    }, [token, user])
+
+    const handleIconClick = (imageUrl: string) => {
+        console.log(imageUrl);
+        setCategoryForm((prev) => ({ ...prev, image_url: imageUrl }));
+    }
     return (
         <div className='flex flex-col bg-gray-200'>
             <div className='grid grid-cols-2'>
@@ -21,18 +47,20 @@ const Categories = () => {
                 <span className='text-center'>收入</span>
             </div>
             <div className='flex flex-col items-center gap-5 p-2'>
-                <div className='w-20 aspect-square border-2 border-yellow-500 bg-white rounded-full'></div>
+                <img src={categoryForm.image_url} className='w-20 p-3 aspect-square border-2 border-yellow-500 bg-white rounded-full' />
                 <input type="text" name='name' value={categoryForm.name} onChange={handleChange} placeholder='請輸入類別名稱' className='border-b text-center focus:outline-none' />
             </div>
 
             <Card className='grid grid-cols-4 content-start place-items-center gap-4 h-full bg-gray-200'>
                 {
-                    Array.from({ length: 20 }).map((_, i) => (
-                        <Icon ></Icon>
+                    categories.map((category, index) => (
+
+                        <Icon key={index} onClick={() => handleIconClick(category.image_url)} isSelected={categoryForm.image_url === category.image_url} image_url={category.image_url}></Icon>
+
                     ))
                 }
             </Card>
-        </div>
+        </div >
     )
 }
 
