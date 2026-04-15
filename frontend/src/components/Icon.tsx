0066f6f0fd58bbc3ea6431fcc15a6388f9ec3cb5
yinghau68 <yinghautsai68cycu@gmail.com>
2026-04-15 23:05:58 +0800

    feat: update Icon to have onClick, isSelected, and image_url props

--- frontend/src/components/Icon.tsx
+++ frontend/src/components/Icon.tsx
@@ -1,8 +1,13 @@
-import React from 'react'
-
-const Icon = () => {
+type IconProps = {
+    onClick: () => void;
+    isSelected: boolean,
+    image_url: string
+}
+const Icon = ({ onClick, isSelected = false, image_url = '' }: IconProps) => {
     return (
-        <div className='w-15 aspect-square  rounded-full bg-white '></div>
+        <div onClick={onClick} className={`${isSelected ? 'border-2 border-yellow-500' : ''} flex flex-row justify-center items-center w-15 p-3 aspect-square  rounded-full bg-white  `}>
+            <img src={image_url} alt="" />
+        </div>
     )
 }
 
