e24769e48aaa4bfc2165df59a9b89bad6111e0d3
yinghau68 <yinghautsai68cycu@gmail.com>
2026-04-16 04:40:53 +0800

    feat: add deleteCategoryById service in frontend

--- frontend/src/services/categories.service.ts
+++ frontend/src/services/categories.service.ts
@@ -83,3 +83,23 @@ export const updateCategoryById = async (token: string, categoryId: string, cate
         throw error;
     }
 }
+
+export const deleteCategoryById = async (token: string, categoryId: number) => {
+    try {
+        const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/categories/${categoryId}`, {
+            method: 'DELETE',
+            headers: {
+                'Authorization': `Bearer ${token}`,
+            },
+        });
+        const result = await response.json();
+        if (!result.success) {
+            throw new Error(result.message);
+        }
+
+        return result.message;
+    } catch (error) {
+        console.error(error)
+        throw error;
+    }
+}
