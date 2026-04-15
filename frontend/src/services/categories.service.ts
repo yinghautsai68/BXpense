c210b0d3b9118963ad25838c8bbfa94576e38a50
yinghau68 <yinghautsai68cycu@gmail.com>
2026-04-16 00:11:37 +0800

    feat: add getCategoryById and updateCategoryById in service

--- frontend/src/services/categories.service.ts
+++ frontend/src/services/categories.service.ts
@@ -1,5 +1,6 @@
 import type { ApiResponse } from "../types/api.types";
 import type { CategoryType, CreateCategoryType } from "../types/categories.type";
+import type { createRecordType } from "../types/records.type";
 
 export const createCategory = async (token: string, categoryForm: CreateCategoryType) => {
     try {
@@ -38,6 +39,47 @@ export const getCategories = async (token: string, userId: string) => {
         return result.data;
     } catch (error) {
         console.error(error);
+        throw error;
+    }
+}
+
+export const getCategoryById = async (token: string, categoryId: string) => {
+    try {
+        const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/categories/${categoryId}`, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        });
+        const result: ApiResponse<CategoryType> = await response.json();
+        if (!result.success) {
+            throw new Error(result.message);
+        }
+        return result.data;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
+
+export const updateCategoryById = async (token: string, categoryId: string, categoryForm: CreateCategoryType) => {
+    try {
+        const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/categories/${categoryId}`, {
+            method: 'PATCH',
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(categoryForm)
+        });
+        const result = await response.json();
+        if (!result.success) {
+            throw new Error(result.message);
+        }
+
+        return result.data;
+    } catch (error) {
+        console.error(error)
         throw error;
     }
 }
