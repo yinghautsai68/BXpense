2edd6f098cf19ef20d256b82339a6948ae424200
yinghau68 <yinghautsai68cycu@gmail.com>
2026-04-16 19:57:04 +0800

    refactor: hide input when input type is 'file'

--- frontend/src/components/FormInput.tsx
+++ frontend/src/components/FormInput.tsx
@@ -15,7 +15,7 @@ const FormInput = React.forwardRef<HTMLInputElement, FormInputProps>(({ classNam
     return (
         <div className='flex flex-col w-full'>
             <label htmlFor={name} className='font-bold'>{label}</label>
-            <input ref={ref} onChange={onChange} id={name} name={name} value={value} type={type} placeholder={placeholder} required={required} className='px-2 py-1 border rounded-lg' />
+            <input ref={ref} onChange={onChange} id={name} name={name} value={value} type={type} placeholder={placeholder} required={required} className={`${type === 'file' ? 'hidden' : ''} px-2 py-1 border rounded-lg`} />
         </div>
     )
 }
