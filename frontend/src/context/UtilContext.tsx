64c60a8868f7ae3fe7cd094c3fc67e44d8a8e4ce
yinghau68 <yinghautsai68cycu@gmail.com>
2026-04-16 21:46:21 +0800

    feat: add formatDate and formatTime helper functions

--- frontend/src/context/UtilContext.tsx
+++ frontend/src/context/UtilContext.tsx
@@ -1,8 +1,10 @@
 import { createContext, useContext } from "react";
 
 type UtilContextType = {
-    formatDateTime: (dateTime: string) => string;
-}
+    formatDate: (dateTime: string) => string,
+    formatTime: (dateTime: string) => string,
+    formatDateTime: (dateTime: string) => string
+};
 
 export const UtilContext = createContext<UtilContextType | null>(null);
 
@@ -10,13 +12,42 @@ type UtilProviderType = {
     children: React.ReactNode
 }
 export const UtilProvider = ({ children }: UtilProviderType) => {
+    const formatDate = (dateTime: string) => {
+        if (!dateTime) {
+            return "";
+        }
+
+        const dateObj = new Date(dateTime);
+        const formatter = new Intl.DateTimeFormat("zh-TW", {
+            year: "numeric",
+            month: "long",
+            day: "2-digit"
+        });
+
+        return formatter.format(dateObj);
+    };
+
+    const formatTime = (dateTime: string) => {
+        if (!dateTime) {
+            return "";
+        }
+
+        const dateObj = new Date(dateTime);
+        const formatter = new Intl.DateTimeFormat("zh-TW", {
+            hour: "2-digit",
+            minute: "2-digit",
+            hour12: true
+        });
+
+        return formatter.format(dateObj);
+    }
+
+
     const formatDateTime = (dateTime: string) => {
         if (!dateTime) {
             return "";
         }
         const dateObj = new Date(dateTime);
-
-
         const formatter = new Intl.DateTimeFormat("zh-TW", {
             year: "numeric",
             month: "long",
@@ -27,13 +58,11 @@ export const UtilProvider = ({ children }: UtilProviderType) => {
             hour12: true
         })
 
-
-
         return formatter.format(dateObj);
     };
 
     return (
-        <UtilContext.Provider value={{ formatDateTime }}>
+        <UtilContext.Provider value={{ formatDate, formatTime, formatDateTime }}>
             {children}
         </UtilContext.Provider>
     );
