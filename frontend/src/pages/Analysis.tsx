ad9f90db5fab49b2a3c85a087bbd13249835b8e5
yinghau68 <yinghautsai68cycu@gmail.com>
2026-04-22 23:22:49 +0800

    fix: fix line data not refetching after change of date

--- frontend/src/pages/Analysis.tsx
+++ frontend/src/pages/Analysis.tsx
@@ -82,7 +82,7 @@ const Analysis = () => {
             }
         }
         fetchData();
-    }, [token]);
+    }, [token, selectedYear, selectedMonth]);
 
     useEffect(() => {
         if (!lineDatas) {
