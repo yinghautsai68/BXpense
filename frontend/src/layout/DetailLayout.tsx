3bf795486a7955129b65c1711f774c2f97610604
yinghau68 <yinghautsai68cycu@gmail.com>
2026-04-25 21:10:54 +0800

    style: update DetailLayout title container to use grid

--- frontend/src/layout/DetailLayout.tsx
+++ frontend/src/layout/DetailLayout.tsx
@@ -11,11 +11,11 @@ const DetailLayout = ({ title, rightAction, children }: DetailLayoutProps) => {
     const navigate = useNavigate();
     return (
         <div className="flex flex-col items-center gap-4 w-full min-h-screen md:p-5 bg-yellow-400  overflow-hidden">
-            <div className='flex flex-col gap-3 w-full flex-1 max-w-md md:h-150 px-5 py-8 bg-gray-100 md:rounded-xl shadow-xl/20 '>
-                <div className="flex flex-row justify-between items-center w-full h-10 px-3">
-                    <p onClick={() => navigate(-1)} className="w-full font-bold cursor-pointer">x</p>
-                    <SubTitle className="w-full text-center">{title}</SubTitle>
-                    <div className="flex flex-row justify-end items-center w-full">
+            <div className='flex flex-col gap-3 w-full flex-1 max-w-md md:h-150 px-5 py-10 bg-gray-100 md:rounded-xl shadow-xl/20 '>
+                <div className="grid grid-cols-[1fr_auto_1fr] w-full h-10  px-3">
+                    <p onClick={() => navigate(-1)} className=" font-bold cursor-pointer">x</p>
+                    <SubTitle className="flex-1 text-center">{title}</SubTitle>
+                    <div className="flex flex-row justify-end items-center">
                         {rightAction ? rightAction : null}
                     </div>
                 </div>
